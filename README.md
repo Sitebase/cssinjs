1092ba03cfc7d24dd97012bc184f3ea615375b4c
Wim <wim@sitebase.be>
2015-02-06 16:14:38 +0100

    initial version

---
+++ README.md
@@ -0,0 +1,12 @@
+React: CSS in JS
+=================
+This repository is created to test out some real life examples and how this would work when using the concept of CSS in JS.
+I based this repository on the [React CSS in JS presentation](http://blog.vjeux.com/2014/javascript/react-css-in-js-nationjs.html) by [Christopher Chedeau](https://github.com/vjeux).
+
+How to use
+----------
+	python -m SimpleHTTPServer
+
+Problems I ran into
+-------------------
+* Hover states(see branch `feature-hover-state`): There is now way to define `:hover` states in inline CSS. So the solution that I'm thinking of is using a interaction aware mixin that will set a `hover` state property to `true` when a user hovers over the item. Possible problems is that the m() expression will become really big.
---
+++ button.jsx
@@ -0,0 +1,58 @@
+var Button = React.createClass({
+  getInitialState: function()
+  {
+    return { test: 'ok' }
+  },
+  styles: {
+    container: {
+      fontSize: '13px',
+      backgroundColor: 'rgb(233, 234, 237)',
+      border: '1px solid #cdced0',
+      borderRadius: 2,
+      boxShadow: '0 1px 1px rgba(0, 0, 0, 0.05)',
+      padding: '0 8px',
+      margin: 2,
+      lineHeight: '23px'
+    },
+    depressed: {
+      backgroundColor: '#4e69a2',
+      borderColor: '#1A356E',
+      color: '#FFF'
+    },
+    smallSize: {
+      fontSize: '10px',
+      lineHeight: '20px'
+    },
+    largeSize: {
+      fontSize: '17px',
+      lineHeight: '30px'
+    },
+    hover: {
+      backgroundColor: '#0F0'
+    }
+  },
+  propTypes: {
+    isDepressed: React.PropTypes.bool,
+    style: React.PropTypes.object,
+    size: React.PropTypes.oneOf(['small', 'default', 'large']),
+  },
+  mixins: [InteractionAwareMixin],
+  getDefaultProps: function() {
+    return {
+      size: 'default'
+    };
+  },
+  render: function() {
+    console.error('Render button with state', this.state);
+    return (
+      <button style={m(
+        this.styles.container,
+        this.props.isDepressed && this.styles.depressed,
+        this.props.size === 'small' && this.styles.smallSize,
+        this.props.size === 'large' && this.styles.largeSize,
+        this.state.interactionHovered && this.styles.hover,
+        this.props.style
+      )}>{this.props.children}</button>
+    );
+  }
+});
